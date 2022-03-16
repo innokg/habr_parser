858d3117e3c793f0bc2a04d073d99238906cebcc
Ulan Chokushov <ulan.chokushov@gmail.com>
2022-03-16 13:38:24 +0600

    ready for deploy

--- habrParser/settings.py
+++ habrParser/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -142,3 +143,5 @@ MEDIA_URL = '/media/'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+django_heroku.settings(locals())
